dc71e38736b343e95ff359db2e92c0315881bcaa
Magnus Rambech <magnusrambech@msn.com>
2019-05-16 01:11:52 +0200

    init

---
+++ precache-manifest.0801b8e409e148d4e4ab9b09eed1e4f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47636b811bb7577cf66d565c3057cfd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d50063259436f39acb1b",
+    "url": "/static/css/main.e6c50332.chunk.css"
+  },
+  {
+    "revision": "e5aa1f41ca51900c7e84",
+    "url": "/static/js/2.14e63729.chunk.js"
+  },
+  {
+    "revision": "d50063259436f39acb1b",
+    "url": "/static/js/main.1d141616.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
