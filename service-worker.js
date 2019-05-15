42990eb157c9aceb63726554452d75f893000bda
Magnus Rambech <magnusrambech@msn.com>
2019-05-16 01:59:07 +0200

    mobile

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0801b8e409e148d4e4ab9b09eed1e4f9.js"
+  "/precache-manifest.13bb144a3ae9f9d531775829994987cb.js"
 );
 
 self.addEventListener('message', (event) => {
